393dce6ad147a600ad18cc28bb367e288c8b493f
dnstrach <dnstrach@gmail.com>
2022-10-23 10:35:06 -0700

    10/18-lab

---
+++ code-intro-solution/index.js
@@ -0,0 +1,91 @@
+/*
+    NOTE: There are multiple ways to solve
+    many of these problems. Your solutions
+    do not have to match exactly.
+*/
+
+///// PROBLEM 1 /////
+
+let speed = 25
+
+
+
+///// PROBLEM 2 /////
+
+speed = 500
+
+
+
+
+///// PROBLEM 3 /////
+
+
+let faveSnack = 'pringles'
+
+
+
+
+///// PROBLEM 4 /////
+
+
+let faveDrink = 'root beer'
+
+
+
+///// PROBLEM 5 /////
+
+const private = true
+
+
+
+
+///// PROBLEM 6 /////
+
+
+let netflix = 15
+let hulu = 7
+let disney = 5
+let youtube = 10
+let peacock = 10
+
+
+
+///// PROBLEM 7 /////
+
+
+let total = netflix + hulu + disney + youtube + peacock
+
+
+
+
+///// PROBLEM 8 /////
+
+hulu = 0
+peacock = 5
+netflix = 8
+
+
+// hulu = hulu - 7
+// peacock = peacock - 5
+// netflix = netflix - 7
+
+// hulu -= 7
+// peacock -= 5
+// netflix -= 7
+
+
+
+///// PROBLEM 9 /////
+
+
+let newTotal = netflix + hulu + disney + youtube + peacock
+
+
+
+///// PROBLEM 10 /////
+
+let savings = (1 - (newTotal / total)) * 100 
+
+
+// let savings = ((total - newTotal) / total) * 100
+
